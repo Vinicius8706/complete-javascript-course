7b2a8d9c5746f5fa5b60b71b686d474103d6cd7e
Vinicius Farias <fsvinicius9@gmail.com>
2022-09-13 12:18:00 -0300

    Var let and const

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,22 @@
-let js = 'amazing';
+let js = "amazing";
 
-if(js === 'amazing') alert('Javascript is amazing')
-      40+8+23-10;
+if (js === "amazing") alert("Javascript is amazing");
+40 + 8 + 23 - 10;
+
+// console.log("Jonas");
+
+// let firstName = "Jonas";
+// let jonas_matilda = "JM";
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+// console.log(typeof true)
+// console.log(typeof 'Jonas')
+// console.log(typeof 23)
+// console.log(typeof null)
+
+let age = 30
+
+const birthYear = 1991;
+
+//var deve ser evitada, utiliza em codigos antigos
